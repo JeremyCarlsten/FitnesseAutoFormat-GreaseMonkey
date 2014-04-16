223ddeb1de068a09d65328fc588209fbc11131e6
JeremyCarlsten <jcc.993@gmail.com>
2014-04-16 08:41:27 -0500

    Adding the script to github

---
+++ src/js/FitnesseAutoFormat.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name           Fitnesse Auto Format
+// @description    Formatting the fitnesse pages when the page saves
+// @author         Jeremy Carlsten
+// @require        http://code.jquery.com/jquery-latest.js
+// @include        http://*FrontPage*
+// @version        1.0
+// ==/UserScript==
+
+var script = document.createElement('script');
+script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+$(document).ready(function(){
+  $formatButton = $(".edit_buttons input[value='Format']");
+  $submitButton = $(".edit_buttons input[value='Save']");
+  $submitButton.click(function(){
+    $formatButton.trigger('click');
+  });
+});
